bba07bca3faea211b39b7105535c20ee591110fd
swh <swh@DESKTOP-OF8AMHR>
2019-06-16 20:44:22 +0900

    y

---
+++ ymg7472/src/Calculator/InsertionSort.java
@@ -0,0 +1,17 @@
+package Calculator;
+
+public class InsertionSort {
+
+	public static void main(String[] args) {
+
+		int [] arr = {4,8,3,1,6,2};
+		for (int i = 1; i<arr.length; i++) {
+			if (i-1 > i) {
+				
+			}
+		}
+		
+
+	}
+
+}
